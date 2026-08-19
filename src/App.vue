772bb20ab5a7eb21e9448f4d8d05c8e8e29d1871
Lukas <04luknie@skola.boras.se>
2026-08-19 13:53:57 +0200

    Removed uneccesary code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import FirstPage from './components/FirstPage.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <FirstPage />
 </template>
---
+++ src/components/FirstPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <section id="center">
+    <button type="button" class="counter" @click="count++">
+      Count is {{ count }}
+    </button>
+  </section>
+  <section id="next-steps">
+
+  </section>
+
+  <section id="spacer"></section>
+</template>
