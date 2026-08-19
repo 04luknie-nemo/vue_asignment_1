27fac156334e9ed70e94207fd7c7c3881d026ffd
Lukas <04luknie@skola.boras.se>
2026-08-19 19:31:35 +0200

    Added form to add movies, visable with console command in browser, added interface and relevant functions with variables

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -2,17 +2,48 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+
+interface Movie {
+  title: string,
+  year: string,
+  plot: string
+}
+const movies = ref<Movie[]>([])
+const newMovie = ref('')
+
+async function addMovie() {
+  if (newMovie.value.trim() === '') return;
+  const response = await fetch(`https://www.omdbapi.com/?apikey=5be74358&t=${newMovie.value}`);
+  const data = await response.json();
+
+  if (data.response === 'False') {
+    console.log('Filmen hittades inte');
+  }
+  movies.value.push({
+    title: data.Title,
+    year: data.Year,
+    plot: data.Plot
+  })
+  newMovie.value = ''
+}
+// @ts-ignore
+window.movies = movies;
+
 </script>
 
 <template>
   <section id="center">
-    <button type="button" class="counter" @click="count++">
-      Count is {{ count }}
-    </button>
+    <article>
+      <button type="button" class="counter" @click="count++">
+        Count is {{ count }}
+      </button>
+      <form @submit.prevent="addMovie">
+        <h2>Lägg Till Film</h2>
+        <div>
+          <input v-model="newMovie" placeholder="Skriv här">
+          <button type="submit">Lägg Till</button>
+        </div>  
+      </form>
+    </article>
   </section>
-  <section id="next-steps">
-
-  </section>
-
-  <section id="spacer"></section>
 </template>
