8ab2660bf3157522ebcd30c6bfff5390f9b258bd
Lukas <04luknie@skola.boras.se>
2026-08-20 17:58:24 +0200

    Here is the new MovieCard component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+interface Movie {
+    title: string,
+    year: string,
+    plot: string
+}
+
+const props = defineProps({
+    movie: {
+        type: Object as () => Movie,
+        required: true
+    }
+})
+</script>
+<template>
+    <article class="movie-card base-card">
+        <p>Titel: {{ movie.title }}</p>
+        <p>Release: {{ movie.year }}</p>
+        <p>Plot: {{ movie.plot }}</p>
+    </article>
+</template>
