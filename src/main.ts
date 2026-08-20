d1b05a354b39eac3a84fcc0d2662f501264a4df7
Lukas <04luknie@skola.boras.se>
2026-08-20 22:04:40 +0200

    Added nav style

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import './cardstyle.css'
 import './pagestyle.css'
+import './routerstyle.css'
 import App from './App.vue'
 import router from './router/index.ts'
 
